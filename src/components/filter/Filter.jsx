31baac6a93255bb901f416288845aa21b4c36200
CiprianR <133102658+Cipick23@users.noreply.github.com>
2024-03-14 07:15:13 +0200

    error filter

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -2,10 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectContacts, selectFiltersContacts } from '../../redux/selectors';
 import { setFilter } from '../../redux/contactsFilterSlice';
 import PropTypes from 'prop-types';
+// import { selectContacts } from '../../redux/selectors';
 
 function Filter() {
   const dispatch = useDispatch();
   const filter = useSelector(selectFiltersContacts);
+  const contacts = useSelector(selectContacts);
+  const isContactsEmpty = contacts === undefined ? true : contacts.length === 0;
 
   const handleFilterChange = event => {
     dispatch(setFilter(event.target.value.trim()));
@@ -18,7 +21,7 @@ function Filter() {
       placeholder="Search by name"
       value={filter}
       onChange={handleFilterChange}
-      disabled={useSelector(selectContacts).length === 0}
+      disabled={isContactsEmpty}
     />
   );
 }
